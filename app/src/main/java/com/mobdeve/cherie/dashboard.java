c4f41832ba7d19e4d21d33229edb42753c29f9c2
DLSYu <hanz_yu@dlsu.edu.ph>
2024-11-24 21:58:23 +0800

    feat: on start dashboard fragment already opened

--- app/src/main/java/com/mobdeve/cherie/dashboard.java
+++ app/src/main/java/com/mobdeve/cherie/dashboard.java
@@ -26,7 +26,7 @@ public class dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-//        replaceFragment(new dashboardFragment());
+        replaceFragment(new dashboardFragment());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
